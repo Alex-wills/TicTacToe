89b243787e8504c86ce4dda58a757fd3664f0396
alex-williams <alex.williams@chetwood.co>
2022-09-12 19:38:40 +0100

    started work on the Board class.

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,95 +1,96 @@
 from random import randrange
 
 
-def create_board():
-    # initialise board
-    # board[row][column]
-    '''
-    [0][0]  [0][1]  [0][2]
+# players class
 
-    [1][0]  [1][1]  [1][2]
+# board class
 
-    [2][0]  [2][1]  [2][2]
-    '''
+class Board:
+    def __init__(self):
+        self.board = [['empty' for column in range(3)] for row in range(3)]
 
-    board = [['empty' for column in range(3)] for row in range(3)]
+    def make_list_of_free_fields(self):
+        # The function browses the board and builds a list of all the free squares;
+        # the list consists of tuples, while each tuple is a pair of row and column numbers.
+        free_fields = []
 
-    spaces = make_list_of_free_fields(board)
+        # scan
+        for row in range(3):
+            for column in range(3):
+                if self.board[row][column] != 'X' and self.board[row][column] != '0':
+                    free_fields.append((row, column))
+                else:
+                    free_fields.append('full')
 
-    for key, value in spaces.items():
-        board[value[0]][value[1]] = key
+        num_to_pos = {}
 
-    board[1][1] = 'X'
-    return board
+        for i in range(len(free_fields)):
+            if free_fields[i] != 'full':
+                num_to_pos[str(i + 1)] = free_fields[i]
 
+        return num_to_pos
 
-def display_board(board):
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
+    def display_board(self):
+        # The function accepts one parameter containing the board's current status
+        # and prints it out to the console.
 
-    print(board)
-    print('+-------+-------+-------+')
-    print('|       |       |       |')
-    print('|  ', board[0][0], '  |  ', board[0][1], '  |  ', board[0][2], '  |')
-    print('|       |       |       |')
-    print('+-------+-------+-------+')
-    print('|       |       |       |')
-    print('|  ', board[1][0], '  |  ', board[1][1], '  |  ', board[1][2], '  |')
-    print('|       |       |       |')
-    print('+-------+-------+-------+')
-    print('|       |       |       |')
-    print('|  ', board[2][0], '  |  ', board[2][1], '  |  ', board[2][2], '  |')
-    print('|       |       |       |')
-    print('+-------+-------+-------+')
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[0][0], '  |  ', self.board[0][1], '  |  ', self.board[0][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[1][0], '  |  ', self.board[1][1], '  |  ', self.board[1][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[2][0], '  |  ', self.board[2][1], '  |  ', self.board[2][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
 
+    def fill_board(self, dic):
+        # initialise board
+        # board[row][column]
+        '''
+        [0][0]  [0][1]  [0][2]
 
-def enter_move(board):
-    # The function accepts the board's current status, asks the user about their move,
-    # checks the input, and updates the board according to the user's decision.
+        [1][0]  [1][1]  [1][2]
 
-    num_to_pos = make_list_of_free_fields(board)
-
-    usr_inp = input('Enter your move: ').upper()
-    move = num_to_pos[usr_inp]
-    board[move[0]][move[1]] = '0'
-
-    return board
-
-
-def draw_move(board):
-    # The function draws the computer's move and updates the board.
-    cpu_inp = randrange(1, 10)
-
-    num_to_pos = make_list_of_free_fields(board)
-
-    while str(cpu_inp) not in num_to_pos:
-        cpu_inp = randrange(1, 10)
-    move = num_to_pos[str(cpu_inp)]
-    board[move[0]][move[1]] = 'X'
-
-    return board
+        [2][0]  [2][1]  [2][2]
+        '''
 
 
-def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares;
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-    free_fields = []
+        for key, value in dic.items():
+            self.board[value[0]][value[1]] = key
 
-    # scan
-    for row in range(3):
-        for column in range(3):
-            if board[row][column] != 'X' and board[row][column] != '0':
-                free_fields.append((row, column))
-            else:
-                free_fields.append('full')
+        self.board[1][1] = 'X'
 
-    num_to_pos = {}
 
-    for i in range(len(free_fields)):
-        if free_fields[i] != 'full':
-            num_to_pos[str(i + 1)] = free_fields[i]
-
-    return num_to_pos
+# def enter_move(board):
+#     # The function accepts the board's current status, asks the user about their move,
+#     # checks the input, and updates the board according to the user's decision.
+#
+#     num_to_pos = make_list_of_free_fields(board)
+#
+#     usr_inp = input('Enter your move: ').upper()
+#     move = num_to_pos[usr_inp]
+#     board[move[0]][move[1]] = '0'
+#
+#     return board
+#
+#
+# def draw_move(board):
+#     # The function draws the computer's move and updates the board.
+#     cpu_inp = randrange(1, 10)
+#
+#     num_to_pos = make_list_of_free_fields(board)
+#
+#     while str(cpu_inp) not in num_to_pos:
+#         cpu_inp = randrange(1, 10)
+#     move = num_to_pos[str(cpu_inp)]
+#     board[move[0]][move[1]] = 'X'
+#
+#     return board
 
 
 # horizontal check
@@ -160,26 +161,32 @@ def victory_for(board, sign):
                 continue
 
 
-board = create_board()
+b = Board()
+spaces = b.make_list_of_free_fields()
+
+b.fill_board(spaces)
+(b.display_board())
+
+
 num_of_moves = 1
 
+# #
+# while num_of_moves < 9:
+#     display_board(board)
+#     enter_move(board)
+#     display_board(board)
+#     if victory_for(board, '0'):
+#         print("Well done you win!!!!!")
+#         break
+#     draw_move(board)
+#     display_board(board)
+#     if victory_for(board, 'X'):
+#         print("Unlucky Computer wins!!!!!")
+#         break
 #
-while num_of_moves < 9:
-    display_board(board)
-    enter_move(board)
-    display_board(board)
-    if victory_for(board, '0'):
-        print("Well done you win!!!!!")
-        break
-    draw_move(board)
-    display_board(board)
-    if victory_for(board, 'X'):
-        print("Unlucky Computer wins!!!!!")
-        break
-
-    num_of_moves += 2
-
-print("*****************************")
-print("*********GAME OVER***********")
-print("*****************************")
-quit()
+#     num_of_moves += 2
+#
+# print("*****************************")
+# print("*********GAME OVER***********")
+# print("*****************************")
+# quit()
