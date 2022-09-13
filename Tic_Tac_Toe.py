da590e7f7a81c0d149ea24819ca8df5c8632c345
alex-williams <alex.williams@chetwood.co>
2022-09-13 11:13:33 +0100

    Refactored game into Board and Player class.

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,13 +1,34 @@
 from random import randrange
 
-
 # players class
+'''whenever you need to interact with board you must step into Board b with b.board'''
+
+''' player methods could be static right now, need to make it so they either are static or use objects sign'''
+
 
 # board class
 
 class Board:
     def __init__(self):
         self.board = [['empty' for column in range(3)] for row in range(3)]
+
+    def display_board(self):
+        # The function accepts one parameter containing the board's current status
+        # and prints it out to the console.
+
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[0][0], '  |  ', self.board[0][1], '  |  ', self.board[0][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[1][0], '  |  ', self.board[1][1], '  |  ', self.board[1][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|  ', self.board[2][0], '  |  ', self.board[2][1], '  |  ', self.board[2][2], '  |')
+        print('|       |       |       |')
+        print('+-------+-------+-------+')
 
     def make_list_of_free_fields(self):
         # The function browses the board and builds a list of all the free squares;
@@ -30,25 +51,7 @@ class Board:
 
         return num_to_pos
 
-    def display_board(self):
-        # The function accepts one parameter containing the board's current status
-        # and prints it out to the console.
-
-        print('+-------+-------+-------+')
-        print('|       |       |       |')
-        print('|  ', self.board[0][0], '  |  ', self.board[0][1], '  |  ', self.board[0][2], '  |')
-        print('|       |       |       |')
-        print('+-------+-------+-------+')
-        print('|       |       |       |')
-        print('|  ', self.board[1][0], '  |  ', self.board[1][1], '  |  ', self.board[1][2], '  |')
-        print('|       |       |       |')
-        print('+-------+-------+-------+')
-        print('|       |       |       |')
-        print('|  ', self.board[2][0], '  |  ', self.board[2][1], '  |  ', self.board[2][2], '  |')
-        print('|       |       |       |')
-        print('+-------+-------+-------+')
-
-    def fill_board(self, dic):
+    def fill_board(self):
         # initialise board
         # board[row][column]
         '''
@@ -59,6 +62,7 @@ class Board:
         [2][0]  [2][1]  [2][2]
         '''
 
+        dic = self.make_list_of_free_fields()
 
         for key, value in dic.items():
             self.board[value[0]][value[1]] = key
@@ -66,127 +70,125 @@ class Board:
         self.board[1][1] = 'X'
 
 
-# def enter_move(board):
-#     # The function accepts the board's current status, asks the user about their move,
-#     # checks the input, and updates the board according to the user's decision.
-#
-#     num_to_pos = make_list_of_free_fields(board)
-#
-#     usr_inp = input('Enter your move: ').upper()
-#     move = num_to_pos[usr_inp]
-#     board[move[0]][move[1]] = '0'
-#
-#     return board
-#
-#
-# def draw_move(board):
-#     # The function draws the computer's move and updates the board.
-#     cpu_inp = randrange(1, 10)
-#
-#     num_to_pos = make_list_of_free_fields(board)
-#
-#     while str(cpu_inp) not in num_to_pos:
-#         cpu_inp = randrange(1, 10)
-#     move = num_to_pos[str(cpu_inp)]
-#     board[move[0]][move[1]] = 'X'
-#
-#     return board
+class Player:
 
+    def __init__(self, sign):
+        self.sign = sign
 
-# horizontal check
-def h_check(board, sign, row, column):
-    if board[row][column + 1] == sign and board[row][column + 2] == sign:
-        return True
-    else:
-        return False
+    def enter_move(self, b, sign):
+        # The function accepts the board's current status, asks the user about their move,
+        # checks the input, and updates the board according to the user's decision.
 
+        num_to_pos = b.make_list_of_free_fields()
 
-# vertical check
-def v_check(board, sign, row, column):
-    if board[row + 1][column] == sign and board[row + 2][column] == sign:
-        return True
-    else:
-        return False
+        usr_inp = input('Enter your move: ').upper()
+        move = num_to_pos[usr_inp]
+        b.board[move[0]][move[1]] = sign
 
+        return b
 
-# positive diagonal check
-def pd_check(board, sign, row, column):
-    if board[row - 1][column + 1] == sign and board[row - 2][column + 2] == sign:
-        return True
-    else:
-        return False
+    def draw_move(self, b, sign):
+        # The function draws the computer's move and updates the board.
+        cpu_inp = randrange(1, 10)
 
+        num_to_pos = b.make_list_of_free_fields()
 
-# negative diagonal check
-def nd_check(board, sign, row, column):
-    if board[row + 1][column + 1] == sign and board[row + 2][column + 2] == sign:
-        return True
-    else:
-        return False
+        while str(cpu_inp) not in num_to_pos:
+            cpu_inp = randrange(1, 10)
+        move = num_to_pos[str(cpu_inp)]
+        b.board[move[0]][move[1]] = sign
 
+        return b
 
-def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if
-    # the player using 'O's or 'X's has won the game
+    def victory_for(self, b, sign):
+        # The function analyzes the board's status in order to check if
+        # the player using 'O's or 'X's has won the game
 
-    win = False
-
-    # negative diagonal checks for top left field
-    if board[0][0] == sign:
-        if nd_check(board, sign, 0, 0):
-            return True
-
-    # positive diagonal checks for bottom left field
-    if board[2][0] == sign:
-        if pd_check(board, sign, 2, 0):
-            return True
-
-    for row in range(3):
-        for column in range(3):
-            # skip the middle as there is no need to check from this
-            if row == 1 and column == 1:
-                continue
-
-            # perform checks
-
-            # vertical checks for top row only
-            if row == 0 and board[row][column] == sign:
-                if v_check(board, sign, 0, column):
-                    return True
-            # horizontal checks for left column only
-            if column == 0 and board[row][column] == sign:
-                if h_check(board, sign, row, 0):
-                    return True
+        # checks####################################################################################
+        # horizontal check
+        def h_check(b, sign, row, column):
+            if b.board[row][column + 1] == sign and b.board[row][column + 2] == sign:
+                return True
             else:
-                continue
+                return False
+
+        # vertical check
+        def v_check(b, sign, row, column):
+            if b.board[row + 1][column] == sign and b.board[row + 2][column] == sign:
+                return True
+            else:
+                return False
+
+        # positive diagonal check
+        def pd_check(b, sign, row, column):
+            if b.board[row - 1][column + 1] == sign and b.board[row - 2][column + 2] == sign:
+                return True
+            else:
+                return False
+
+        # negative diagonal check
+        def nd_check(b, sign, row, column):
+            if b.board[row + 1][column + 1] == sign and b.board[row + 2][column + 2] == sign:
+                return True
+            else:
+                return False
+
+        ##########################################################################################
+
+        # negative diagonal checks for top left field
+        if b.board[0][0] == sign:
+            if nd_check(b, sign, 0, 0):
+                return True
+
+        # positive diagonal checks for bottom left field
+        if b.board[2][0] == sign:
+            if pd_check(b, sign, 2, 0):
+                return True
+
+        for row in range(3):
+            for column in range(3):
+                # skip the middle as there is no need to check from this
+                if row == 1 and column == 1:
+                    continue
+
+                # perform checks
+
+                # vertical checks for top row only
+                if row == 0 and b.board[row][column] == sign:
+                    if v_check(b, sign, 0, column):
+                        return True
+                # horizontal checks for left column only
+                if column == 0 and b.board[row][column] == sign:
+                    if h_check(b, sign, row, 0):
+                        return True
+                else:
+                    continue
 
 
-b = Board()
-spaces = b.make_list_of_free_fields()
+if __name__ == '__main__':
+    user = Player('0')
+    cpu = Player('X')
+    b = Board()
+    b.fill_board()
+    num_of_moves = 1
 
-b.fill_board(spaces)
-(b.display_board())
+    #
+    while num_of_moves < 9:
+        b.display_board()
+        user.enter_move(b, user.sign)
+        b.display_board()
+        if user.victory_for(b, user.sign):
+            print("Well done you win!!!!!")
+            break
+        cpu.draw_move(b, cpu.sign)
+        b.display_board()
+        if cpu.victory_for(b, cpu.sign):
+            print("Unlucky Computer wins!!!!!")
+            break
 
+        num_of_moves += 2
 
-num_of_moves = 1
-
-# #
-# while num_of_moves < 9:
-#     display_board(board)
-#     enter_move(board)
-#     display_board(board)
-#     if victory_for(board, '0'):
-#         print("Well done you win!!!!!")
-#         break
-#     draw_move(board)
-#     display_board(board)
-#     if victory_for(board, 'X'):
-#         print("Unlucky Computer wins!!!!!")
-#         break
-#
-#     num_of_moves += 2
-#
-# print("*****************************")
-# print("*********GAME OVER***********")
-# print("*****************************")
-# quit()
+    print("*****************************")
+    print("*********GAME OVER***********")
+    print("*****************************")
+    quit()
--- Tic_tests.py
+++ Tic_tests.py
@@ -1,75 +1,70 @@
 import unittest
 
 # would usually be a class so fewer references
-from Tic_Tac_Toe import victory_for, create_board
+from Tic_Tac_Toe import Board, Player
+
 
 class TestTicTacToe(unittest.TestCase):
 
     def test_left_vert_win(self):
-        board = create_board()
+        b = Board()
+        user = Player('0')
 
-        board[0][0] = '0'
-        board[1][0] = '0'
-        board[2][0] = '0'
+        b.board[0][0] = '0'
+        b.board[1][0] = '0'
+        b.board[2][0] = '0'
 
-        victory_for(board, '0')
-
-        self.assertEqual(victory_for(board, '0'), True)
+        self.assertEqual(user.victory_for(b, '0'), True)
 
     def test_right_vert_win(self):
-        board = create_board()
+        b = Board()
+        user = Player('0')
 
-        board[0][2] = '0'
-        board[1][2] = '0'
-        board[2][2] = '0'
+        b.board[0][2] = '0'
+        b.board[1][2] = '0'
+        b.board[2][2] = '0'
 
-        victory_for(board, '0')
-
-        self.assertEqual(victory_for(board, '0'), True)
+        self.assertEqual(user.victory_for(b, '0'), True)
 
     def test_top_hori_win(self):
-        board = create_board()
+        b = Board()
+        user = Player('0')
 
-        board[0][0] = '0'
-        board[0][1] = '0'
-        board[0][2] = '0'
+        b.board[0][0] = '0'
+        b.board[0][1] = '0'
+        b.board[0][2] = '0'
 
-        victory_for(board, '0')
-
-        self.assertEqual(victory_for(board, '0'), True)
+        self.assertEqual(user.victory_for(b, '0'), True)
 
     def test_bottom_hori_win(self):
-        board = create_board()
+        b = Board()
+        user = Player('0')
 
-        board[2][0] = '0'
-        board[2][1] = '0'
-        board[2][2] = '0'
+        b.board[2][0] = '0'
+        b.board[2][1] = '0'
+        b.board[2][2] = '0'
 
-        victory_for(board, '0')
-
-        self.assertEqual(victory_for(board, '0'), True)
+        self.assertEqual(user.victory_for(b, '0'), True)
 
     def test_nd_win(self):
-        board = create_board()
+        b = Board()
+        cpu = Player('X')
 
-        board[0][0] = 'X'
-        board[1][1] = 'X'
-        board[2][2] = 'X'
+        b.board[0][0] = 'X'
+        b.board[1][1] = 'X'
+        b.board[2][2] = 'X'
 
-        victory_for(board, 'X')
-
-        self.assertEqual(victory_for(board, 'X'), True)
+        self.assertEqual(cpu.victory_for(b, 'X'), True)
 
     def test_pd_win(self):
-        board = create_board()
+        b = Board()
+        cpu = Player('X')
 
-        board[2][0] = 'X'
-        board[1][1] = 'X'
-        board[0][2] = 'X'
+        b.board[2][0] = 'X'
+        b.board[1][1] = 'X'
+        b.board[0][2] = 'X'
 
-        victory_for(board, 'X')
-
-        self.assertEqual(victory_for(board, 'X'), True)
+        self.assertEqual(cpu.victory_for(b, 'X'), True)
 
 
 if __name__ == '__main__':
